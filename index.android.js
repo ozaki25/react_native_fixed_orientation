d7d8e6769477330c4731c3cf03a17ae9d5357cf4
ozaki <fs65ugumori@yahoo.co.jp>
2017-09-27 07:45:59 +0900

    初回画面の固定がきいてなかっｔ対応

--- index.android.js
+++ index.android.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import Orientation from 'react-native-orientation';
 import AppNavigator from './app/navigators/AppNavigator';
 import { onNavigationStateChange } from './app/utils/Utils';
 
-const ReactNativeSample = () => <AppNavigator onNavigationStateChange={onNavigationStateChange} />
+class ReactNativeSample extends Component {
+  componentWillMount() {
+    Orientation.lockToPortrait();
+  }
+
+  render() {
+    return <AppNavigator onNavigationStateChange={onNavigationStateChange} />;
+  }
+}
 
 AppRegistry.registerComponent('ReactNativeSample', () => ReactNativeSample);
